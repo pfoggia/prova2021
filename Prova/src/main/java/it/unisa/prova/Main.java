a164be730324be90edc68c9c70510158dd7849ac
Pasquale Foggia <pfoggia@unisa.it>
2021-11-09 11:30:32 +0100

    Primo commit

---
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -0,0 +1,12 @@
+
+package it.unisa.prova;
+
+/**
+ *
+ * @author foggia
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello, world");
+    }
+}
